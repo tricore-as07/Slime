f9be92a5ac641c630185b489be93dc4a860db069
tricore-as07 <tricore.as07@gmail.com>
2020-10-09 12:25:19 +0900

    フックアクションの時の加速させるクラスのリファクタリング

--- Assets/MyGameAssets/Scripts/Stage/AddMotorForce.cs
+++ Assets/MyGameAssets/Scripts/Stage/AddMotorForce.cs
@@ -5,32 +5,42 @@
 /// </summary>
 public class AddMotorForce : MonoBehaviour
 {
-    [SerializeField] HingeJoint joint = default;
-    [SerializeField] float force = 0f;
-    [SerializeField] float targetVelocity = 0f;
-    [SerializeField] float deadZoneVelocity = 0f;
+    [SerializeField] HingeJoint joint = default;                //フックを繋げるためのクラス
+    [SerializeField] float force = 0f;                          //加速する力
+    [SerializeField] float targetVelocity = 0f;                 //加速する目標
+    [SerializeField] float deadZoneVelocity = 0f;               //加速しない時の速度（速度が一定以下になると逆方向に加速させるため）
 
-    // Update is called once per frame
+    /// <summary>
+    /// 毎フレーム呼び出される
+    /// </summary>
     void Update()
     {
+        // 繋がっているオブジェクトがある時
         if(joint.connectedBody != null)
         {
+            // モータークラスを取得
             var motor = joint.motor;
+            // xがプラス方向の速度が加速しない時の速度より大きい時
             if (joint.connectedBody.velocity.x > deadZoneVelocity)
             {
+                // xのプラス方向に加速させる
                 motor.force = force;
                 motor.targetVelocity = -targetVelocity;
             }
-            else if (joint.connectedBody.velocity.x < -deadZoneVelocity)
+            // xがマイナス方向の速度が加速しない時の速度より大きい時
+            else if (-joint.connectedBody.velocity.x > deadZoneVelocity)
             {
+                // xのマイナス方向に加速させる
                 motor.force = force;
                 motor.targetVelocity = targetVelocity;
             }
+            // 速度が小さすぎる場合は加速しない
             else
             {
                 motor.force = 0f;
                 motor.targetVelocity = 0f;
             }
+            // 計算したモータークラスの情報に書き換え
             joint.motor = motor;
         }
     }

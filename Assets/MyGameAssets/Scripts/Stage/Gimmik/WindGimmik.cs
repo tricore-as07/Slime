66db1b4f37b82f9bdd91553c1245d5532327ebeb
tricore-as07 <tricore.as07@gmail.com>
2020-10-13 16:23:55 +0900

    コメントの追加

--- Assets/MyGameAssets/Scripts/Stage/Gimmik/WindGimmik.cs
+++ Assets/MyGameAssets/Scripts/Stage/Gimmik/WindGimmik.cs
@@ -15,12 +15,17 @@ public class WindGimmik : MonoBehaviour
     /// <param name="other">この衝突に含まれるその他のCollider</param>
     void OnTriggerStay(Collider other)
     {
+        // 衝突したオブジェクトがプレイヤーなら
         if (other.gameObject.tag == TagName.Player)
         {
+            // Rigidbodyをキャッシュ
             playerRigidbody = playerRigidbody ?? other.gameObject.GetComponent<Rigidbody>();
+            // 上方向に風の強さの力を加える
             playerRigidbody.AddForce(Vector3.up * WindPower);
+            // プレイヤーの速度が最大速度より大きいなら
             if(playerRigidbody.velocity.y > limitSpeed)
             {
+                // プレイヤーの上方向の速度だけ最大速度に抑える
                 playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, limitSpeed, 0f);
             }
         }

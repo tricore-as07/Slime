61f458cf8fa361e749dff9a5a7bb592544fd3490
tricore-as07 <tricore.as07@gmail.com>
2020-10-13 13:05:18 +0900

    風ギミックを仮作成

---
+++ Assets/MyGameAssets/Scripts/Stage/Gimmik/WindGimmik.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindGimmik : MonoBehaviour
+{
+    [SerializeField] float WindPower = 0f;
+    Rigidbody playerRigidbody;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == TagName.Player)
+        {
+            playerRigidbody = playerRigidbody ?? collision.gameObject.GetComponent<Rigidbody>();
+            playerRigidbody.AddForce(Vector3.up * WindPower);
+        }
+    }
+}

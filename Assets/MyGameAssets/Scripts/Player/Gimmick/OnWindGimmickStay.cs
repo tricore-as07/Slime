4101326cad23dda3459124c1ee2022689d58972b
tricore-as07 <tricore.as07@gmail.com>
2020-10-16 15:25:13 +0900

    風の影響をプレイヤー自分で力を加える形に変更

--- Assets/MyGameAssets/Scripts/Player/Gimmick/OnWindGimmickStay.cs
+++ Assets/MyGameAssets/Scripts/Player/Gimmick/OnWindGimmickStay.cs
@@ -3,11 +3,11 @@
 /// <summary>
 /// 風のギミックの処理を行う
 /// </summary>
-public class WindGimmick : MonoBehaviour
+public class OnWindGimmickStay : MonoBehaviour
 {
     [SerializeField] float WindPower = 0f;      //風の強さ（加速度）
     [SerializeField] float limitSpeed = 0f;     //風で加速させる最大の速度
-    Rigidbody playerRigidbody;                  //プレイヤーのRigidbody
+    [SerializeField] Rigidbody playerRigidbody; //プレイヤーのRigidbody
 
     /// <summary>
     /// ２つのColliderが衝突している最中に呼び出される（片方はisTriggerがtrueである時）
@@ -16,14 +16,14 @@ public class WindGimmick : MonoBehaviour
     void OnTriggerStay(Collider other)
     {
         // 衝突したオブジェクトがプレイヤーなら
-        if (other.gameObject.tag == TagName.Player)
+        if (other.gameObject.tag == TagName.WindGimmick)
         {
             // Rigidbodyをキャッシュ
-            playerRigidbody = playerRigidbody ?? other.gameObject.GetComponent<Rigidbody>();
+            playerRigidbody = playerRigidbody ?? GetComponent<Rigidbody>();
             // 上方向に風の強さの力を加える
             playerRigidbody.AddForce(Vector3.up * WindPower);
             // プレイヤーの速度が最大速度より大きいなら
-            if(playerRigidbody.velocity.y > limitSpeed)
+            if (playerRigidbody.velocity.y > limitSpeed)
             {
                 // プレイヤーの上方向の速度だけ最大速度に抑える
                 playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, limitSpeed, 0f);

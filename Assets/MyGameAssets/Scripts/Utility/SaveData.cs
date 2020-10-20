9ba447ef5ae006a3162172ea03d6d189d9147ccc
tricore-as07 <tricore.as07@gmail.com>
2020-10-20 16:22:49 +0900

    セーブデータクラスをリファクタリング

--- Assets/MyGameAssets/Scripts/Utility/SaveData.cs
+++ Assets/MyGameAssets/Scripts/Utility/SaveData.cs
@@ -1,56 +1,78 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using System;
 using VMUnityLib;
 
+/// <summary>
+/// セーブデータに関する処理をするクラス
+/// </summary>
 [Serializable]
 public class SaveData : SingletonMonoBehaviour<SaveData>
 {
-    [SerializeField]
-    List<StageData> stages = default;
+    [SerializeField] List<StageSaveData> stageSaveDataList = default;       //ステージ毎のセーブデータのリスト
 
-    void Start()
+    /// <summary>
+    /// スクリプトのインスタンスがロードされたときに呼び出される
+    /// </summary>
+    void Awake()
     {
+        // セーブデータのファイルが存在しているなら
         if (JsonDataSaver.FileExists<SaveData>(this))
         {
+            // セーブデータのファイルが存在しているならそのファイルをロードする
             JsonDataSaver.Load<SaveData>(this);
-            if (stages == null)
+            // ロードしたファイルのステージのリストがnullなら
+            if (stageSaveDataList == null)
             {
-                stages = new List<StageData>();
+                // リストを初期化する
+                stageSaveDataList = new List<StageSaveData>();
             }
         }
+        // セーブデータのファイルが存在しないなら
         else
         {
+            InitializationSaveData();
+            // 初期化したセーブデータを保存してファイルを作成しておく
             JsonDataSaver.Save<SaveData>(this);
         }
     }
 
     /// <summary>
-    /// セーブをする
+    /// セーブデータを初期化する
+    /// </summary>
+    void InitializationSaveData()
+    {
+        stageSaveDataList = new List<StageSaveData>();
+    }
+
+    /// <summary>
+    /// ステージのデータをセーブする
     /// </summary>
     /// <param name="stageNum">ステージ数</param>
     /// <param name="isAcquireList">ステージにあるダイヤを取ったかどうかをリストでまとめたもの</param>
-    void Save(int stageNum, IReadOnlyCollection<bool> isAcquireList)
+    void SaveStageData(int stageNum, IReadOnlyCollection<bool> isAcquireList)
     {
         // 既にセーブされてるステージのリストがこれからセーブするステージ数分ないなら
         // 今のステージ数に達するまで追加する
-        while(stages.Count < stageNum)
+        while(stageSaveDataList.Count < stageNum)
         {
-            stages = new List<StageData>();
+            stageSaveDataList = new List<StageSaveData>();
         }
         // 新しいリストの作成
-        StageData newData = new StageData();
-        newData.isAcquireList = new List<bool>();
+        StageSaveData newStageSaveData = new StageSaveData();
+        newStageSaveData.isAcquireItemList = new List<bool>();
         // 新しいリストにダイヤを取ったかどうかのリストをまとめて追加
-        newData.isAcquireList.AddRange(isAcquireList);
+        newStageSaveData.isAcquireItemList.AddRange(isAcquireList);
         // 新しく作ったリストで上書きする
-        stages[stageNum - 1] = newData;
+        stageSaveDataList[stageNum - 1] = newStageSaveData;
     }
 }
 
+/// <summary>
+/// ステージのセーブデータ
+/// </summary>
 [Serializable]
-public class StageData
+public class StageSaveData
 {
-    public List<bool> isAcquireList = default;
+    public List<bool> isAcquireItemList = default;          //アイテム獲得したかどうかのリスト
 }

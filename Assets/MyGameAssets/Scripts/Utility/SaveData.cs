5654a14e1f20bece63d2a97cdf033f258c344d21
tricore-as07 <tricore.as07@gmail.com>
2020-10-21 12:36:49 +0900

    セーブ関連のリファクタリング

--- Assets/MyGameAssets/Scripts/Utility/SaveData.cs
+++ Assets/MyGameAssets/Scripts/Utility/SaveData.cs
@@ -1,117 +1,79 @@
 ﻿using System.Collections.Generic;
-using UnityEngine;
 using System;
-using VMUnityLib;
 
 /// <summary>
 /// セーブデータに関する処理をするクラス
 /// </summary>
 [Serializable]
-public class SaveData : SingletonMonoBehaviour<SaveData>
+public class SaveData
 {
-    [SerializeField] Root root;       //ステージ毎のセーブデータのリスト
+    public StageSaveData stageSaveData = default;                               //ステージのセーブデータ
 
     /// <summary>
-    /// スクリプトのインスタンスがロードされたときに呼び出される
+    /// デフォルトコンストラクタ
     /// </summary>
-    void Awake()
+    public SaveData()
     {
-        // セーブデータのファイルが存在しているなら
-        if (JsonDataSaver.FileExists<Root>(root))
-        {
-            // セーブデータのファイルが存在しているならそのファイルをロードする
-            root = JsonDataSaver.Load<Root>(root);
-            // ロードしたファイルのステージのリストがnullなら
-            if (root == null)
-            {
-                InitializationSaveData();
-            }
-        }
-        // セーブデータのファイルが存在しないなら
-        else
-        {
-            InitializationSaveData();
-            // 初期化したセーブデータを保存してファイルを作成しておく
-            JsonDataSaver.Save<Root>(root);
-        }
+        stageSaveData = new StageSaveData();
     }
-
-    /// <summary>
-    /// セーブデータを初期化する
-    /// </summary>
-    void InitializationSaveData()
-    {
-        root = new Root();
-        root.stageData = new StageSaveDataList();
-        root.stageData.stageSaveDataList = new List<StageSaveData>();
-        root.stageData.stageSaveDataList.Add(new StageSaveData());
-        root.stageData.stageSaveDataList[0].isAcquireItemList = new List<bool>();
-        root.stageData.stageSaveDataList[0].isAcquireItemList.Add(false);
-    }
-
-    /// <summary>
-    /// ステージのデータをセーブする
-    /// </summary>
-    /// <param name="stageNum">ステージ数</param>
-    /// <param name="isAcquireList">ステージにあるダイヤを取ったかどうかをリストでまとめたもの</param>
-    public void SaveStageData(int stageNum, IReadOnlyCollection<bool> isAcquireList)
-    {
-        // 既にセーブされてるステージのリストがこれからセーブするステージ数分ないなら
-        // 今のステージ数に達するまで追加する
-        while(root.stageData.stageSaveDataList.Count < stageNum)
-        {
-            root.stageData.stageSaveDataList = new List<StageSaveData>();
-        }
-        // 新しいリストの作成
-        StageSaveData newStageSaveData = new StageSaveData();
-        newStageSaveData.isAcquireItemList = new List<bool>();
-        // 新しいリストにダイヤを取ったかどうかのリストをまとめて追加
-        newStageSaveData.isAcquireItemList.AddRange(isAcquireList);
-        // 新しく作ったリストで上書きする
-        root.stageData.stageSaveDataList[stageNum - 1] = newStageSaveData;
-    }
-
-    /// <summary>
-    /// ステージのセーブデータを返す
-    /// </summary>
-    /// <param name="stageNum"></param>
-    /// <returns></returns>
-    public StageSaveData LoadStageData(int stageNum)
-    {
-        if(root.stageData.stageSaveDataList.Count >= stageNum)
-        {
-            StageSaveData stageSaveData = root.stageData.stageSaveDataList[stageNum - 1].Clone() as StageSaveData;
-            return stageSaveData;
-        }
-        return new StageSaveData();
-    }
-}
-
-[Serializable]
-public class Root
-{
-    public StageSaveDataList stageData;       //ステージ毎のセーブデータのリスト
 }
 
 /// <summary>
 /// ステージ毎のセーブデータをリストで管理する
 /// </summary>
 [Serializable]
-public class StageSaveDataList
+public class StageSaveData
 {
-    public List<StageSaveData> stageSaveDataList = default;
+    public List<DiamondAcquisitionData> diamondAcquisitionDataList = default;   //ステージのセーブデータのリスト
+
+    /// <summary>
+    /// デフォルトコンストラクタ
+    /// </summary>
+    public StageSaveData()
+    {
+        // リストの初期化
+        diamondAcquisitionDataList = new List<DiamondAcquisitionData>();
+        // 何もしないでセーブした時のためにデータを一つ追加しておく
+        diamondAcquisitionDataList.Add(new DiamondAcquisitionData());
+    }
+
+    /// <summary>
+    /// コピーコンストラクタ
+    /// </summary>
+    /// <param name="stageSaveData">コピー元のStageSaveDataクラス</param>
+    public StageSaveData(StageSaveData stageSaveData)
+    {
+        // 引数のインスタンスを元にステージのセーブデータのリストをコピーする
+        this.diamondAcquisitionDataList = new List<DiamondAcquisitionData>(stageSaveData.diamondAcquisitionDataList);
+    }
 }
 
 /// <summary>
-/// ステージのセーブデータ
+/// ダイアモンドを獲得したかどうかのリストをラッピンングするデータクラス
 /// </summary>
 [Serializable]
-public class StageSaveData : ICloneable
+public class DiamondAcquisitionData
 {
-    public List<bool> isAcquireItemList = default;          //アイテム獲得したかどうかのリスト
+    public List<bool> isDiamondAcquisitionList = default;                       //アイテム獲得したかどうかのリスト
 
-    public object Clone()
+    /// <summary>
+    /// デフォルトコンストラクタ
+    /// </summary>
+    public DiamondAcquisitionData()
     {
-        return MemberwiseClone();
+        // リストの初期化
+        isDiamondAcquisitionList = new List<bool>();
+        // 何もしないでセーブした時のためにデータを一つ追加しておく
+        isDiamondAcquisitionList.Add(false);
+    }
+
+    /// <summary>
+    /// コピーコンストラクタ
+    /// </summary>
+    /// <param name="diamondAcquisitionData">コピー元のDiamondAcquisitionDataクラス</param>
+    public DiamondAcquisitionData(DiamondAcquisitionData diamondAcquisitionData)
+    {
+        // 引数のインスタンスを元にアイテム獲得したかどうかのリストをコピーする
+        this.isDiamondAcquisitionList = new List<bool>(diamondAcquisitionData.isDiamondAcquisitionList);
     }
 }

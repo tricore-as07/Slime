ca35044383d29cf2829e0b16104d6b3616069d1c
tricore-as07 <tricore.as07@gmail.com>
2020-11-25 11:13:22 +0900

    広告外しボタンとサウンドの切り替えスイッチの仮実装

---
+++ Assets/MyGameAssets/Scripts/UI/AdButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Purchasing;
+
+/// <summary>
+/// 広告削除ボタンの処理
+/// </summary>
+public class AdButton : MonoBehaviour
+{
+    /// <summary>
+    /// Updateが最初に呼び出される前のフレームで呼び出される
+    /// </summary>
+    void Start()
+    {
+        //IAPの初期化が完了していたら
+        if (CodelessIAPStoreListener.initializationComplete)
+        {
+            // slimedeladが購入されているかを調べる
+            var product = CodelessIAPStoreListener.Instance.GetProduct("slimedelad");
+            // slimedeladが購入されたレシートが存在しているなら
+            if (product.hasReceipt)
+            {
+                // 広告削除ボタンを非表示にする
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+    /// <summary>
+    /// 購入処理が完了したときに呼ばれる
+    /// </summary>
+    public void OnPurchaseComplete(Product product)
+    {
+        //this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/MyGameAssets/Scripts/UI/SoundSwitchButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// サウンドのオンオフを切り替えるボタンの処理
+/// </summary>
+public class SoundSwitchButton : MonoBehaviour
+{
+    [SerializeField] GameObject switchingDestination;       //切り替え先
+
+    /// <summary>
+    /// 切り替えボタンを押された時の処理
+    /// </summary>
+    public void OnClickSwitchButton()
+    {
+        switchingDestination.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}

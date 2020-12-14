7b66ccbf54d459e4eec3366d50f2bae3544b25d5
tricore-as07 <tricore.as07@gmail.com>
2020-12-14 16:35:57 +0900

    言語設定ボタン用のシステムを忘れていたので実装

---
+++ Assets/MyGameAssets/Scripts/UI/SelectLanguageButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using I2.Loc;
+
+/// <summary>
+/// 言語設定用のボタン
+/// </summary>
+public class SelectLanguageButton : MonoBehaviour
+{
+    [SerializeField] string languageName = default;     //ボタンが押された時に設定する言語の名前
+
+    /// <summary>
+    /// ボタンがクリックされた時
+    /// </summary>
+    public void OnClickButton()
+    {
+        LocalizationManager.CurrentLanguage = languageName;
+    }
+}

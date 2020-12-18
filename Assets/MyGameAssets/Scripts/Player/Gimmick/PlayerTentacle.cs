a9b82f701333075ecdea95556969f67d7ccabd75
tricore-as07 <tricore.as07@gmail.com>
2020-12-18 16:06:31 +0900

    触手のブラッシュアップ

--- Assets/MyGameAssets/Scripts/Player/Gimmick/PlayerTentacle.cs
+++ Assets/MyGameAssets/Scripts/Player/Gimmick/PlayerTentacle.cs
@@ -5,13 +5,15 @@
 /// </summary>
 public class PlayerTentacle : MonoBehaviour
 {
-    GameObject player;          //プレイヤーのゲームオブジェクト
-    Vector3 hook;               //フックのポジション
-    Vector3 mediumPos;          //プレイヤーのポジションとフックのポジションの中間
-    float dist;                 //プレイヤーとフックの距離
-    float extendTime;           //触手を伸ばすのにかかる時間
-    float extendElapsedTime;    //触手を伸ばすのにかかった経過時間
-    bool endExtendTentacle;     //触手を伸ばし終えたかどうか 
+    GameObject player;                              //プレイヤーのゲームオブジェクト
+    Vector3 hook;                                   //フックのポジション
+    Vector3 mediumPos;                              //プレイヤーのポジションとフックのポジションの中間
+    float dist;                                     //プレイヤーとフックの距離
+    float extendTime;                               //触手を伸ばすのにかかる時間
+    float extendElapsedTime;                        //触手を伸ばすのにかかった経過時間
+    bool endExtendTentacle;                         //触手を伸ばし終えたかどうか
+    float tentacleMaxThickness;                        //触手の太さ
+    [SerializeField] MeshRenderer mesh = default;   //メッシュコンポーネント
 
     /// <summary>
     /// 毎フレーム呼び出される
@@ -30,7 +32,9 @@ public class PlayerTentacle : MonoBehaviour
     /// <param name="playerObject">プレイヤーのゲームオブジェクト</param>
     /// <param name="hookPosition">フックのポジション</param>
     /// <param name="argExtendTime">フックを伸ばすのにかかる時間</param>
-    public void ExtendTentacle(GameObject playerObject,Vector3 hookPosition,float argExtendTime)
+    /// <param name="material">プレイヤーの見た目のマテリアル</param>
+    /// <param name="tentacleMaxThickness">触手の最大の太さ</param>
+    public void ExtendTentacle(GameObject playerObject,Vector3 hookPosition,float argExtendTime,Material material,float tentacleMaxThickness)
     {
         endExtendTentacle = false;
         extendElapsedTime = 0f;
@@ -38,6 +42,8 @@ public class PlayerTentacle : MonoBehaviour
         player = playerObject;
         hook = hookPosition;
         gameObject.SetActive(true);
+        mesh.material = material;
+        this.tentacleMaxThickness = tentacleMaxThickness;
         FixTentacle();
     }
 
@@ -59,6 +65,7 @@ public class PlayerTentacle : MonoBehaviour
         extendElapsedTime += Time.deltaTime;
         //経過時間でプレイヤーからフックまでのどのくらいの割合まで触手を伸ばすかを決定する
         float extendPercentage = extendElapsedTime / extendTime;
+        float tentacleThickness;
         dist = Vector3.Distance(player.transform.position, hook);
         //伸ばす割合が１以上なら
         if (extendPercentage >= 1)
@@ -72,6 +79,7 @@ public class PlayerTentacle : MonoBehaviour
             }
             // フックとプレイヤーの中間のポジションを求める
             mediumPos = (player.transform.position + hook) * 0.5f;
+            tentacleThickness = 0.1f;
         }
         else
         {
@@ -79,10 +87,11 @@ public class PlayerTentacle : MonoBehaviour
             dist *= extendPercentage;
             // 触手の長さから触手のオブジェクトの中心位置を求める
             mediumPos = player.transform.position + (hook - player.transform.position) * extendPercentage * 0.5f;
+            tentacleThickness = 0.1f * (tentacleMaxThickness / extendPercentage);
         }
         // 各パラメータを決定
         transform.position = mediumPos;
-        transform.localScale = new Vector3(0.1f, 0.1f, dist);
+        transform.localScale = new Vector3(tentacleThickness, tentacleThickness, dist);
         transform.LookAt(hook);
     }
 }
--- Assets/MyGameAssets/Scripts/Utility/PlayerSettingsData.cs
+++ Assets/MyGameAssets/Scripts/Utility/PlayerSettingsData.cs
@@ -28,5 +28,7 @@ public class PlayerSettingsData : ScriptableObject
     [InfoBox("タップの押された瞬間を\r\n判定する時の誤差許容範囲", InfoMessageType.None, 0)]
     [SerializeField] float tapErrorToleranceTime = 0f;                          //タップされた瞬間を判定する時の誤差の許容範囲
     public float TapErrorToleranceTime => tapErrorToleranceTime;                //外部に公開するためのプロパティ
-
+    [InfoBox("触手の最大の太さ", InfoMessageType.None, 0)]
+    [SerializeField] float tentacleMaxThickness = 1.0f;                         //タップされた瞬間を判定する時の誤差の許容範囲
+    public float TentacleMaxThickness => tentacleMaxThickness;                  //外部に公開するためのプロパティ
 }

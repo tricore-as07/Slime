973058e4c2817d885c9f9f1d03522a65fe693aa7
tricore-as07 <tricore.as07@gmail.com>
2021-03-16 17:07:24 +0900

    UIの調整

---
+++ Assets/MyGameAssets/Scripts/UI/FadeTMP.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FadeTMP : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text = default;
+    [SerializeField, Range(0, 1f)] float minAlpha = 0;
+    [SerializeField, Range(0, 1f)] float maxAlpha = 0;
+    [SerializeField] float fadeTime = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Fade());
+    }
+
+    IEnumerator Fade()
+    {
+        bool fadeOut = false;
+        Color nowColor = default;
+        while (true)
+        {
+            nowColor = text.color;
+            if (fadeOut)
+            {
+                nowColor.a -= Time.deltaTime / fadeTime;
+                text.color = nowColor;
+                if(nowColor.a <= minAlpha)
+                {
+                    fadeOut = false;
+                }
+            }
+            else
+            {
+                nowColor.a += Time.deltaTime / fadeTime;
+                text.color = nowColor;
+                if (nowColor.a >= maxAlpha)
+                {
+                    fadeOut = true;
+                }
+            }
+            yield return null;
+        }
+    }
+}
